fix mass scaling issue

env.py
@@ -45,11 +45,11 @@ class ControlSuiteEnv():
     self.x_pos = 0
 
     if self.initial_body_mass is None:
-      self.initial_body_mass = self._env.physics.model.body_mass
+      self.initial_body_mass = self._env.physics.model.body_mass.copy()
     else:
       self._env.physics.model.body_mass[:] = self.initial_body_mass
     if self.initial_color is None:
-      self.initial_color = self._env.physics.model.geom_rgba[1:]
+      self.initial_color = self._env.physics.model.geom_rgba[1:].copy()
     else:
       self._env.physics.model.geom_rgba[1:] = self.initial_color
     
@@ -61,8 +61,6 @@ class ControlSuiteEnv():
     state = self._env.reset()
     self._env.physics.model.body_mass[:] = self.initial_body_mass
     self._env.physics.model.geom_rgba[1:] = self.initial_color
-    if self.x_pos > 10:
-      import pdb; pdb.set_trace()
     
     state = torch.tensor([self.get_x_pos()]).unsqueeze(0)
     if self.symbolic:
@@ -85,7 +83,6 @@ class ControlSuiteEnv():
     x_pos = self.get_x_pos()
     if self.distribution_shift == 'mass':
       if x_pos > TRANSITION_POINT:
-        import pdb; pdb.set_trace()
         self.x_pos = x_pos
         self._env.physics.model.body_mass[:] = self.initial_body_mass * MASS_SCALE_FACTOR
       else:

main.py
@@ -388,11 +388,11 @@ for episode in tqdm(range(metrics['episodes'][-1] + 1, args.episodes + 1), total
 
   writer.add_scalar("train_reward", metrics['train_rewards'][-1], metrics['steps'][-1])
   writer.add_scalar("train/episode_reward", metrics['train_rewards'][-1], metrics['steps'][-1]*args.action_repeat)
-  writer.add_scalar("observation_loss",  np.mean(metrics['observation_loss'][0]), metrics['steps'][-1])
-  writer.add_scalar("reward_loss", np.mean(metrics['reward_loss'][0]), metrics['steps'][-1])
-  writer.add_scalar("kl_loss",  np.mean(metrics['kl_loss'][0]), metrics['steps'][-1])
-  writer.add_scalar("actor_loss",  np.mean(metrics['actor_loss'][0]), metrics['steps'][-1])
-  writer.add_scalar("value_loss",  np.mean(metrics['value_loss'][0]), metrics['steps'][-1])
+  writer.add_scalar("observation_loss",  np.mean(metrics['observation_loss'][-1]), metrics['steps'][-1])
+  writer.add_scalar("reward_loss", np.mean(metrics['reward_loss'][-1]), metrics['steps'][-1])
+  writer.add_scalar("kl_loss",  np.mean(metrics['kl_loss'][-1]), metrics['steps'][-1])
+  writer.add_scalar("actor_loss",  np.mean(metrics['actor_loss'][-1]), metrics['steps'][-1])
+  writer.add_scalar("value_loss",  np.mean(metrics['value_loss'][-1]), metrics['steps'][-1])
   writer.add_scalar("x_pos", metrics['x_pos'][-1], metrics['steps'][-1])
   writer.add_scalar("mass", metrics['mass'][-1], metrics['steps'][-1])
 